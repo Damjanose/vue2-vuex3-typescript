b365dc8866620aba3628f8a0bced43e7615d8575
Damjano Semini <damjanoda@gmail.com>
2023-01-31 13:39:49 +0100

    fix #hash in url when navigate,  - use store to save input value in licalstorage and save the value if page reload

--- vue2_typescript/src/router/index.ts
+++ vue2_typescript/src/router/index.ts
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 export default new VueRouter({
+  mode: 'history',
   routes: [
     { path: '/', name: 'LoginView', component: () => import(/* webpackChunkName: "login-view" */ '../views/LoginView.vue') },
     { path: '/register', name: 'RegisterView', component: () => import(/* webpackChunkName: "register-view" */ '../views/RegisterView.vue') },
--- vue2_typescript/src/store/index.ts
+++ vue2_typescript/src/store/index.ts
@@ -5,13 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    text: localStorage.getItem('text2'),
   },
   getters: {
+    text({ text }) {
+      console.log(text)
+      return text || ''
+    },
   },
   mutations: {
+    SET_TEXT(state, payload) {
+      state.text = payload
+    },
   },
   actions: {
+    setText({ commit }, payload) {
+      localStorage.setItem('text2', payload)
+      commit('SET_TEXT', payload)
+    },
   },
   modules: {
-  }
+  },
 })
